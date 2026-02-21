9777c1e0dfb7637edb46cf0886a1be624ab356c3
Haniya <ummehaniya111@gmail.com>
2026-02-21 22:59:16 +0530

    bind flask to render port

--- app.py
+++ app.py
@@ -162,8 +162,12 @@ def uploaded_file(filename):
 # if __name__ == "__main__":
 #     app.run()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 application = app
 
